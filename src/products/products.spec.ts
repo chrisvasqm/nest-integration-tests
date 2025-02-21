04fbd2f8a2d2c58d1e2a633f090a13e52906349a
chrisvasqm <christianvasquezmelo@gmail.com>
2025-02-21 09:23:03 -0400

    Add integration tests

---
+++ src/products/products.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { ProductsModule } from './products.module';
+import { Server } from 'http';
+import * as request from 'supertest';
+import { Product } from './product.model';
+
+describe('ProductsService', () => {
+  let app: INestApplication;
+  let server: Server;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ProductsModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+    server = app.getHttpServer() as Server;
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET /products', () => {
+    it('should return an array', async () => {
+      const response = await request(server).get('/products');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('POST /products', () => {
+    it('should add a product', async () => {
+      const product = {
+        name: 'Product A',
+        price: 9.99,
+      };
+
+      const postResponse = await request(server)
+        .post('/products')
+        .send(product);
+
+      expect(postResponse.status).toBe(201);
+      expect(postResponse.body).toHaveProperty('id');
+      expect(postResponse.body).toMatchObject(product);
+
+      const getResponse = await request(server).get('/products');
+
+      expect(getResponse.status).toBe(200);
+      const products = getResponse.body as Array<Product>;
+      expect(products.length).toBe(1);
+    });
+  });
+});
